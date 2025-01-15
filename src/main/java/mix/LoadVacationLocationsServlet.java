7adb4b75a2f396faf2192b3e31f7e3ce2933a0ab
Maria-Teodora Popescu <popescumariateodora@yahoo.com>
2025-01-15 16:03:28 +0200

    fara arond ok?

--- src/main/java/mix/LoadVacationLocationsServlet.java
+++ src/main/java/mix/LoadVacationLocationsServlet.java
@@ -14,7 +14,7 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import org.json.JSONArray;
 
-@WebServlet("/LoadVacationLocationsServlet")
+// @WebServlet("/LoadVacationLocationsServlet")
 public class LoadVacationLocationsServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
